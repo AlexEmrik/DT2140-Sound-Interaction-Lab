53e932ef2cc1d3bd337dbcfb95ca80c8e9a2189b
Alexander Karolin <alexander.karolin@hotmail.com>
2025-11-19 12:03:00 +0100

    back to engine

--- interaction-2.js
+++ interaction-2.js
@@ -12,7 +12,7 @@ let dspNodeParams = null;
 let jsonParams = null;
 
 // Change here to ("tuono") depending on your wasm file name
-const dspName = "bells";
+const dspName = "engine";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
@@ -126,8 +126,8 @@ function playAudioEngine() {
     if (audioContext.state === 'suspended') {
         return;
     }
-    dspNode.setParamValue("/englishBell/gate", 1)
-    setTimeout(() => { dspNode.setParamValue("/englishBell/gate", 0) }, 100);
+    dspNode.setParamValue("/engine/gate", 1)
+    setTimeout(() => { dspNode.setParamValue("/engine/gate", 0) }, 200);
 }
 
 //==========================================================================================
