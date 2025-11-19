45de7d26309f289fed978df196d9c8dbc10cf460
Alexander Karolin <alexander.karolin@hotmail.com>
2025-11-19 14:13:33 +0100

    wind

--- interaction-3.js
+++ interaction-3.js
@@ -12,7 +12,7 @@ let dspNodeParams = null;
 let jsonParams = null;
 
 // Change here to ("tuono") depending on your wasm file name
-const dspName = "Door";
+const dspName = "wind";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
@@ -25,7 +25,7 @@ if (typeof module === "undefined") {
 }
 
 // The name should be the same as the WASM file, so change tuono with brass if you use brass.wasm
-Door.createDSP(audioContext, 1024)
+wind.createDSP(audioContext, 1024)
     .then(node => {
         dspNode = node;
         dspNode.connect(audioContext.destination);
@@ -56,12 +56,6 @@ function accelerationChange(accx, accy, accz) {
 }
 
 function rotationChange(rotx, roty, rotz) {
-    if(roty>0){
-        dspNode.setParamValue("/door/door/positon", 0.2);
-    }
-    else {
-        dspNode.setParamValue("/door/door/positon", 0);
-    }
 
 }
 
